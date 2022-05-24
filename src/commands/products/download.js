df3ac97d6fb6782bd6f03edb359fdefe17d00847
Bryan Morris <bryan@internalfx.com>
2022-05-24 06:10:36 -0500

    Support ignorefiles in products

--- src/commands/products/download.js
+++ src/commands/products/download.js
@@ -2,28 +2,66 @@
 import _ from 'lodash'
 import Promise from 'bluebird'
 import { log, getTarget, mkdir, stringifyProduct } from '../../helpers.js'
+import ignoreParser from 'gitignore-parser'
 import path from 'path'
 import fsp from 'fs/promises'
 import requestify from '../../requestify.js'
 import context from '../../context.js'
 
 export default async function (argv) {
+  let ignore = null
   const config = context.config
   const target = await getTarget(config, argv)
 
   let total = 0
+  const filter = argv.filter ? new RegExp(`^${argv.filter}`) : null
 
-  const processProducts = async function (products) {
+  try {
+    const ignoreFile = await fsp.readFile(`.quickshot-ignore`, `utf8`)
+    ignore = ignoreParser.compile(ignoreFile)
+  } catch (err) {}
+
+  let res = await requestify(target, {
+    method: `get`,
+    url: `/products.json`,
+    qs: {
+      limit: 100,
+    },
+  })
+
+  let done = false
+
+  if (_.get(res, `body.products.length`) === 0) {
+    done = true
+  }
+
+  await mkdir(path.join(process.cwd(), `products`))
+
+  while (done === false) {
+    let products = _.get(res, `body.products`)
+
+    if (ignore) {
+      products = _.reject(products, function (page) {
+        return ignore.denies(`pages/${page.handle}`)
+      })
+    }
+
+    if (filter) {
+      products = _.filter(products, function (page) {
+        return filter.test(`pages/${page.handle}`)
+      })
+    }
+
     await Promise.map(products, async function (product) {
       const res = await requestify(target, {
         method: `get`,
-        url: `/products/${product.id}/metafields.json`
+        url: `/products/${product.id}/metafields.json`,
       })
 
       const metafields = _.get(res, `body.metafields`)
 
       product.metafields = metafields.map(function (metafield) {
-        return _.pick(metafield, `namespace`, `key`, `value`, `value_type`)
+        return _.pick(metafield, `key`, `namespace`, `value`, `type`, `description`)
       })
 
       await fsp.writeFile(path.join(process.cwd(), `products`, `${product.handle}.html`), stringifyProduct(product))
@@ -32,32 +70,9 @@ export default async function (argv) {
 
       await log(`Product "${product.handle}" downloaded`, `green`)
     })
-  }
-
-  let res = await requestify(target, {
-    method: `get`,
-    url: `/products.json`,
-    qs: {
-      limit: 100
-    }
-  })
-
-  let productsDone = false
-
-  if (_.get(res, `body.products.length`) === 0) {
-    productsDone = true
-  }
-
-  await mkdir(path.join(process.cwd(), `products`))
-
-  // Process Products
-  while (productsDone === false) {
-    const products = _.get(res, `body.products`)
-
-    await processProducts(products)
 
     if (res.linkNext == null) {
-      productsDone = true
+      done = true
       break
     }
 
@@ -67,7 +82,7 @@ export default async function (argv) {
       qs: {
         limit: 100,
         page_info: res.linkNext.searchParams.get(`page_info`)
-      }
+      },
     })
   }
 
--- src/commands/products/upload.js
+++ src/commands/products/upload.js
@@ -1,33 +1,68 @@
 
 import _ from 'lodash'
 import Promise from 'bluebird'
-import { log, getTarget, parseArticle, parseProduct } from '../../helpers.js'
+import { log, getTarget, parseProduct } from '../../helpers.js'
+import ignoreParser from 'gitignore-parser'
+import path from 'path'
 import fsp from 'fs/promises'
 import requestify from '../../requestify.js'
 import glob from 'glob'
 import context from '../../context.js'
 
 export default async function (argv) {
+  let ignore = null
   const config = context.config
   const target = await getTarget(config, argv)
 
   let total = 0
+  const filter = argv.filter ? new RegExp(`^${argv.filter}`) : null
 
-  const productFilePaths = glob.sync(`products/*.html`, { nodir: true })
+  try {
+    const ignoreFile = await fsp.readFile(`.quickshot-ignore`, `utf8`)
+    ignore = ignoreParser.compile(ignoreFile)
+  } catch (err) {}
 
-  await Promise.map(productFilePaths, async function (productFilePath) {
-    const productSource = await fsp.readFile(productFilePath, `utf8`)
-    const product = parseProduct(productSource)
+  let files = glob.sync(`products/*.html`, { nodir: true })
+
+  if (ignore) {
+    files = _.reject(files, function (file) {
+      return ignore.denies(file)
+    })
+  }
+
+  if (filter) {
+    files = _.filter(files, function (file) {
+      const pathParts = file.split(path.sep)
+      const trimmedParts = _.drop(pathParts, (_.lastIndexOf(pathParts, `products`) + 1))
+      const key = trimmedParts.join(`/`)
+      return filter.test(key)
+    })
+  }
+
+  files = files.map((file) => {
+    const pathParts = file.split(path.sep)
+    const trimmedParts = _.drop(pathParts, (_.lastIndexOf(pathParts, `products`) + 1))
+    const filepath = trimmedParts.join(path.sep)
+
+    return {
+      key: filepath,
+      name: path.basename(filepath),
+      path: file
+    }
+  })
+
+  await Promise.map(files, async function (file) {
+    const source = await fsp.readFile(file.path, `utf8`)
+    const product = parseProduct(source)
     const metafields = product.metafields
     delete product.metafields
 
-    // Get shopify product
     let res = await requestify(target, {
       method: `get`,
       url: `/products.json`,
       qs: {
-        handle: product.handle
-      }
+        handle: product.handle,
+      },
     })
 
     let shopifyProduct = _.get(res, `body.products[0]`)
@@ -39,16 +74,16 @@ export default async function (argv) {
         method: `put`,
         url: `/products/${product.id}.json`,
         body: {
-          product: product
-        }
+          product,
+        },
       })
     } else {
       res = await requestify(target, {
         method: `post`,
         url: `/products.json`,
         body: {
-          product: product
-        }
+          product,
+        },
       })
 
       shopifyProduct = _.get(res, `body.product`)
@@ -56,14 +91,15 @@ export default async function (argv) {
 
     res = await requestify(target, {
       method: `get`,
-      url: `/products/${shopifyProduct.id}/metafields.json`
+      url: `/products/${shopifyProduct.id}/metafields.json`,
     })
 
     const shopifyMetafields = _.get(res, `body.metafields`)
 
     await Promise.map(metafields, async function (metafield) {
       const shopifyMetafield = shopifyMetafields.find(function (shopifyMetafield) {
-        return shopifyMetafield.namespace === metafield.namespace && shopifyMetafield.key === metafield.key
+        return shopifyMetafield.namespace === metafield.namespace &&
+        shopifyMetafield.key === metafield.key
       })
 
       if (shopifyMetafield != null) {
@@ -73,16 +109,16 @@ export default async function (argv) {
           method: `put`,
           url: `/products/${shopifyProduct.id}/metafields/${metafield.id}.json`,
           body: {
-            metafield: metafield
-          }
+            metafield: metafield,
+          },
         })
       } else {
         res = await requestify(target, {
           method: `post`,
           url: `/products/${shopifyProduct.id}/metafields.json`,
           body: {
-            metafield: metafield
-          }
+            metafield: metafield,
+          },
         })
       }
     })
@@ -90,7 +126,7 @@ export default async function (argv) {
     total += 1
 
     await log(`Product "${product.handle}" uploaded`, `green`)
-  }, { concurrency: 8 })
+  }, { concurrency: config.concurrency })
 
   return `Uploaded ${total} products.`
 }
